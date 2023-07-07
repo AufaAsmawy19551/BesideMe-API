2758367e2331c05add6f1ef9e35f59a6d698befd
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-07-07 16:33:13 +0700

    update

---
+++ index.mjs
@@ -0,0 +1,10 @@
+import app from "./app.mjs"
+import dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.PORT || '3000';
+
+app.listen(port, () => {
+  console.log(`Application is running on port ${port}`);
+  }
+)
